b20d6029b9178527760edab211782589732252fa
admin <62morozov.dima.90@gmail.com>
2024-12-10 09:50:17 +0300

    first commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+import static javax.swing.JOptionPane.*;
+public class Demo {
+    public static void main(String[] args) {
+        //текстоые переменные:
+        String txt, str;
+        //
+        txt = showInputDialog("Введите текст:");
+
+        System.out.println("Hello");
+    }
+}
